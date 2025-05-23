1cd7b3b39fe8cac6cffd24a23f5ef5542d40b725
01-J-L <johnlesterolingay951827@gmail.com>
2025-05-23 08:47:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = create_app()
 
 
 if __name__ == '__main__':
+    debug_mode = os.getenv('FLASK_DEBUG', 'True').lower() in ('true', '1', 't')
+    
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port, debug=debug_mode)
 
 
