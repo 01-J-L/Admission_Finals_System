de5b14f099c3c74c9ab602e79b0df5f29c30c5ad
01-J-L <johnlesterolingay951827@gmail.com>
2025-05-29 23:39:58 +0800

    Your commit message here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from website import create_app
 from dotenv import load_dotenv
-import os
 load_dotenv() 
 
 app = create_app()
@@ -8,9 +7,6 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    debug_mode = os.getenv('FLASK_DEBUG', 'True').lower() in ('true', '1', 't')
-    
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    app.run(debug=True)
 
 
