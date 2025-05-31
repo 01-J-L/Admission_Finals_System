4bf521c296fb91e4664c0360e5564a011fd8d186
01-J-L <johnlesterolingay951827@gmail.com>
2025-05-31 22:15:58 +0800

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -2,14 +2,7 @@ import os
 from main import app
 from flask import Flask
 
-from whitenoise import WhiteNoise
 
-application = WhiteNoise(app)
-app = Flask(__name__, static_folder='static')
-
-
-static_files_directory = os.path.join(app.root_path, app.static_folder)
-application.add_files(static_files_directory, prefix=app.static_url_path)
 
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 4000))
