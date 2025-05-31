e1f731d9b69e38046e67eb1cd6ad7c04834f8e85
01-J-L <johnlesterolingay951827@gmail.com>
2025-05-31 21:34:48 +0800

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 import os
 from main import app
+from flask import Flask
 
 from whitenoise import WhiteNoise
 
 application = WhiteNoise(app)
+app = Flask(__name__, static_folder='static')
+
 
 static_files_directory = os.path.join(app.root_path, app.static_folder)
 application.add_files(static_files_directory, prefix=app.static_url_path)
